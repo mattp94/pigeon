7cd6f15fddd111d0aa6ca1b422b32a9a3edd687c
mattp94 <matthieu.puibaraud@gmail.com>
2021-01-06 13:10:45 +0100

    feat(api): handle cors

--- packages/api/lib/app.js
+++ packages/api/lib/app.js
@@ -1,12 +1,13 @@
 const Koa = require("koa");
 
 const auth = require("./middlewares/auth");
+const cors = require("./middlewares/cors");
 const logger = require("./middlewares/logger");
 const message = require("./middlewares/message");
 const validator = require("./middlewares/validator");
 
 const app = new Koa();
 
-app.use(logger).use(validator).use(auth).use(message);
+app.use(cors).use(logger).use(validator).use(auth).use(message);
 
 module.exports = app;
