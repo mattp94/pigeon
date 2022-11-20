64f2dfd49467a1c0ef0e4fba45bad4ea65969e75
mattp94 <matthieu.puibaraud@gmail.com>
2022-11-20 20:02:10 +0100

    feat(cli): add figures package

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
 const chalk = require("chalk");
+const figures = require("figures");
 
 const send = require("./lib/send");
 
 send()
-  .then(() => console.log(chalk.green("✔"), "Sent"))
+  .then(() => console.log(chalk.green(figures.tick), "Sent"))
   .catch((err) => {
-    console.error(chalk.red("✘"), err.message);
+    console.error(chalk.red(figures.cross), err.message);
     process.exit(1);
   });
